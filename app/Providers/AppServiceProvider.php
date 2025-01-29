fabe3f9d938bb2af1c13db1dc31671e11fe91bd9
CandidoRPNeto <candido.neto@ucsal.edu.br>
2025-01-29 09:10:20 -0300

    add project original fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // $proxy_url = config('route.proxy_url');
+        // $proxy_scheme = config('route.proxy_scheme');
+
+        // if (!empty($proxy_url)) {
+        //     URL::forceRootUrl($proxy_url);
+        // }
+
+        // if (!empty($proxy_scheme)) {
+        //     URL::forceScheme($proxy_scheme);
+        // }
     }
 }
