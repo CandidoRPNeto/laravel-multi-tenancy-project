1139286c1cd539d07a27ce77efd5701d9f8633dd
CandidoRPNeto <candido.neto@ucsal.edu.br>
2025-02-03 08:08:12 -0300

    start selector on blade

---
+++ resources/views/components/ai-select-options.blade.php
@@ -0,0 +1,10 @@
+<div class="">
+    <label for="provider">Provider:</label>
+    <select class="bottom-full mb-1" name="provider">
+        <option>1</option>
+    </select>
+    <label for="model">Model:</label>
+    <select class="bottom-full mb-1" name="model">
+        <option>1</option>
+    </select>
+</div>
