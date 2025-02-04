18459d3c17fb672c3148c30ba0ee8cab8dba6ce3
CandidoRPNeto <candido.neto@ucsal.edu.br>
2025-02-04 10:07:18 -0300

    Turn company id scopes into a trait

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,21 +2,19 @@
 
 namespace App\Models;
 
-use App\Models\Scopes\CompanyScope;
+use App\BelongsToCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Attributes\ScopedBy;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-#[ScopedBy([CompanyScope::class])]
 class Client extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, BelongsToCompany;
 
     protected $fillable = [
+        'company_id',
         'address_id',
         'user_id'
     ];
