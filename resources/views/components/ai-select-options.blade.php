d36e744eb770c2b14cc7f2acfde383e1f1959744
CandidoRPNeto <candido.neto@ucsal.edu.br>
2025-02-04 08:59:15 -0300

    Add company scope

--- resources/views/components/ai-select-options.blade.php
+++ resources/views/components/ai-select-options.blade.php
@@ -1,10 +1,10 @@
-<div class="">
+<div class="text-white">
     <label for="provider">Provider:</label>
-    <select class="bottom-full mb-1" name="provider">
+    <select class="bottom-full mr-3 mb-1 bg-transparent" name="provider">
         <option>1</option>
     </select>
     <label for="model">Model:</label>
-    <select class="bottom-full mb-1" name="model">
+    <select class="bottom-full mb-1 bg-transparent" name="model">
         <option>1</option>
     </select>
 </div>
